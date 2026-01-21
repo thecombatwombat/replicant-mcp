0c7ca86eeb359a4a948456f05c457c50a09c538f
Archit Joshi <archit.joshi@gmail.com>
2026-01-20 20:00:10 -0800

    chore: add adapters and parsers barrel exports

---
+++ src/adapters/index.ts
@@ -0,0 +1,4 @@
+export { AdbAdapter } from "./adb.js";
+export { EmulatorAdapter, type EmulatorListResult } from "./emulator.js";
+export { GradleAdapter } from "./gradle.js";
+export { UiAutomatorAdapter } from "./ui-automator.js";
---
+++ src/parsers/index.ts
@@ -0,0 +1,4 @@
+export * from "./adb-output.js";
+export * from "./emulator-output.js";
+export * from "./gradle-output.js";
+export * from "./ui-dump.js";
